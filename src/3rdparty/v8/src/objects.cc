191ff6f02a3eb9d2b036be5121a905044ffcfd0d
Thiago Macieira <thiago.macieira@intel.com>
2011-09-01 11:30:40 +0200

    [V8] Fix compilation with Intel CC.
    Issue reported upstream at:
    http://code.google.com/p/v8/issues/detail?id=1885
    http://code.google.com/p/v8/issues/detail?id=689
    
    Change-Id: I1ef47ded9bcc13174bfb82ff04dec230b7ba68e7
    Reviewed-by: Kent Hansen <kent.hansen@nokia.com>
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/3rdparty/v8/src/objects.cc
+++ src/3rdparty/v8/src/objects.cc
@@ -12146,6 +12146,7 @@ template class Dictionary<SeededNumberDictionaryShape, uint32_t>;
 
 template class Dictionary<UnseededNumberDictionaryShape, uint32_t>;
 
+#ifndef __INTEL_COMPILER
 template MaybeObject* Dictionary<SeededNumberDictionaryShape, uint32_t>::
     Allocate(int at_least_space_for);
 
@@ -12238,7 +12239,7 @@ int Dictionary<StringDictionaryShape, String*>::NumberOfEnumElements();
 
 template
 int HashTable<SeededNumberDictionaryShape, uint32_t>::FindEntry(uint32_t);
-
+#endif
 
 // Collates undefined and unexisting elements below limit from position
 // zero of the elements. The object stays in Dictionary mode.
