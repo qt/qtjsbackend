cf3296721194eafb9bb07ee94026cfb5bcc5d092
Kai Koehne <kai.koehne@nokia.com>
2011-11-10 16:00:37 +0100

    [V8] Add flag to avoid breakpoint relocation
    Add a flag that prevents v8 from relocating breakpoints across
    line boundaries.
    
    Change-Id: Ida410b2d7f0ffa3edcf76ce2ec2614497f68f3f3
    Reviewed-by: Simon Hausmann <simon.hausmann@nokia.com>

--- src/3rdparty/v8/src/flag-definitions.h
+++ src/3rdparty/v8/src/flag-definitions.h
@@ -229,6 +229,7 @@ DEFINE_bool(trace_debug_json, false, "trace debugging JSON request/response")
 DEFINE_bool(debugger_auto_break, true,
             "automatically set the debug break flag when debugger commands are "
             "in the queue")
+DEFINE_bool(breakpoint_relocation, true, "relocate breakpoints to the next executable line")
 DEFINE_bool(enable_liveedit, true, "enable liveedit experimental feature")
 
 // execution.cc
