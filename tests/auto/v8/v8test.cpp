qt/qtjsbackend

commit c511fa8a6a631e45ee4075453bcb2eeb7f01ba63

Let V8 throw the exception when QML property lookup fails
This should be squashed into the "Introduce a QML compilation mode"
commit (8a4d849a0152c76bd9107a1b38a641cf8c0ff226) the next time V8
is rebased.

In normal (non-QML) mode, when a global property lookup fails
(implicit receiver ("this") object), V8 decides to throw an exception
iff the expression evaluation is not part of a "typeof" evaluation.
If it is evaluated as part of "typeof", the result silently becomes
"undefined". This is the expected behavior as per the ECMA-262
standard; "typeof" shouldn't throw a ReferenceError even if the
expression involves a global variable that doesn't exist.

This commit brings the QML mode behavior in line with normal mode.
When the receiver object is a QML global object, V8 up until now
hasn't detected whether it should throw an error in that case. The
QML implementation has been working around that by explicitly
throwing the ReferenceError in the QML context wrapper, but that
breaks the "typeof" operator in QML mode. The QML context wrapper
should rather return an empty handle, and leave it up to V8 to
throw the exception as appropriate.

This also reverts the parts of the original QML mode patch that
changed the RelocInfo mode for QML variables, since V8 relies
precisely on this mode to know whether it's evaluating a "typeof".
(It's no longer clear why the RelocInfo mode was modified in the
first place.)

Task-number: QTBUG-24448
Change-Id: Ic33610d5e91bdf373b22d97a3181e6e5f2fc1843
Reviewed-by: Aaron Kennedy <aaron.kennedy@nokia.com>

tests/auto/v8/tst_v8.cpp
@@ -60,6 +60,8 @@ private slots:
     void externalteardown();
     void globalcall();
     void getcallingqmlglobal();
+    void typeOf();
+    void referenceerror();
 };
 
 void tst_v8::eval()
@@ -90,6 +92,16 @@ void tst_v8::globalcall()
 void tst_v8::getcallingqmlglobal()
 {
     QVERIFY(v8test_getcallingqmlglobal());
+}
+
+void tst_v8::typeOf()
+{
+    QVERIFY(v8test_typeof());
+}
+
+void tst_v8::referenceerror()
+{
+    QVERIFY(v8test_referenceerror());
 }
 
 int main(int argc, char *argv[])

tests/auto/v8/v8test.cpp
@@ -432,5 +432,66 @@ cleanup:
     ENDTEST();
 }
 
+bool v8test_typeof()
+{
+    BEGINTEST();
+
+    HandleScope handle_scope;
+    Persistent<Context> context = Context::New();
+    Context::Scope context_scope(context);
+
+    Local<Object> qmlglobal = Object::New();
+    qmlglobal->Set(String::New("a"), Integer::New(123));
+
+    Local<Script> script = Script::Compile(String::New("["
+                                                       "typeof a === 'number', "
+                                                       "typeof b === 'undefined', "
+                                                       "(function() { return typeof c === 'undefined'; })()"
+                                                       "]"), NULL, NULL,
+                                           Handle<String>(), Script::QmlMode);
+
+    TryCatch tc;
+    Local<Value> result = script->Run(qmlglobal);
+
+    VERIFY(!tc.HasCaught());
+    VERIFY(result->IsArray());
+    VERIFY(v8::Array::Cast(*result)->Length() == 3);
+    VERIFY(v8::Array::Cast(*result)->Get(0)->IsTrue());
+    VERIFY(v8::Array::Cast(*result)->Get(1)->IsTrue());
+    VERIFY(v8::Array::Cast(*result)->Get(2)->IsTrue());
+
+cleanup:
+    context.Dispose();
+
+    ENDTEST();
+}
+
+bool v8test_referenceerror()
+{
+    BEGINTEST();
+
+    HandleScope handle_scope;
+    Persistent<Context> context = Context::New();
+    Context::Scope context_scope(context);
+
+    Local<Object> qmlglobal = Object::New();
+
+    Local<Script> script = Script::Compile(String::New("a"), NULL, NULL,
+                                           Handle<String>(), Script::QmlMode);
+
+    TryCatch tc;
+    Local<Value> result = script->Run(qmlglobal);
+
+    VERIFY(tc.HasCaught());
+    VERIFY(result.IsEmpty());
+    VERIFY(tc.Exception()->IsError());
+    VERIFY(tc.Exception()->ToString()->Equals(v8::String::New("ReferenceError: a is not defined")));
+
+cleanup:
+    context.Dispose();
+
+    ENDTEST();
+}
+
 #undef VARNAME
 #undef VARVALUE

tests/auto/v8/v8test.h
@@ -54,6 +54,8 @@ bool v8test_userobjectcompare();
 bool v8test_externalteardown();
 bool v8test_globalcall();
 bool v8test_getcallingqmlglobal();
+bool v8test_typeof();
+bool v8test_referenceerror();
 
 #endif // V8TEST_H
 

src/3rdparty/v8/src/arm/full-codegen-arm.cc
@@ -1185,7 +1185,7 @@ void FullCodeGenerator::EmitLoadGlobalCheckExtensions(Variable* var,
 
   __ ldr(r0, var->is_qml_global()?QmlGlobalObjectOperand():GlobalObjectOperand());
   __ mov(r2, Operand(var->name()));
-  RelocInfo::Mode mode = (typeof_state == INSIDE_TYPEOF || var->is_qml_global())
+  RelocInfo::Mode mode = (typeof_state == INSIDE_TYPEOF)
       ? RelocInfo::CODE_TARGET
       : RelocInfo::CODE_TARGET_CONTEXT;
   Handle<Code> ic = isolate()->builtins()->LoadIC_Initialize();
@@ -1273,7 +1273,7 @@ void FullCodeGenerator::EmitVariableLoad(VariableProxy* proxy) {
       __ ldr(r0, var->is_qml_global()?QmlGlobalObjectOperand():GlobalObjectOperand());
       __ mov(r2, Operand(var->name()));
       Handle<Code> ic = isolate()->builtins()->LoadIC_Initialize();
-      __ Call(ic, var->is_qml_global()?RelocInfo::CODE_TARGET:RelocInfo::CODE_TARGET_CONTEXT);
+      __ Call(ic, RelocInfo::CODE_TARGET_CONTEXT);
       context()->Plug(r0);
       break;
     }
@@ -2271,7 +2271,7 @@ void FullCodeGenerator::VisitCall(Call* expr) {
     // Push global object as receiver for the call IC.
     __ ldr(r0, proxy->var()->is_qml_global()?QmlGlobalObjectOperand():GlobalObjectOperand());
     __ push(r0);
-    EmitCallWithIC(expr, proxy->name(), proxy->var()->is_qml_global()?RelocInfo::CODE_TARGET:RelocInfo::CODE_TARGET_CONTEXT);
+    EmitCallWithIC(expr, proxy->name(), RelocInfo::CODE_TARGET_CONTEXT);
   } else if (proxy != NULL && proxy->var()->IsLookupSlot()) {
     // Call to a lookup slot (dynamically introduced variable).
     Label slow, done;

src/3rdparty/v8/src/arm/lithium-codegen-arm.cc
@@ -3281,7 +3281,7 @@ void LCodeGen::DoCallGlobal(LCallGlobal* instr) {
   ASSERT(ToRegister(instr->result()).is(r0));
 
   int arity = instr->arity();
-  RelocInfo::Mode mode = instr->qml_global()?RelocInfo::CODE_TARGET:RelocInfo::CODE_TARGET_CONTEXT;
+  RelocInfo::Mode mode = RelocInfo::CODE_TARGET_CONTEXT;
   Handle<Code> ic =
       isolate()->stub_cache()->ComputeCallInitialize(arity, mode);
   __ mov(r2, Operand(instr->name()));

src/3rdparty/v8/src/ia32/full-codegen-ia32.cc
@@ -1140,7 +1140,7 @@ void FullCodeGenerator::EmitLoadGlobalCheckExtensions(Variable* var,
   __ mov(eax, var->is_qml_global()?QmlGlobalObjectOperand():GlobalObjectOperand());
   __ mov(ecx, var->name());
   Handle<Code> ic = isolate()->builtins()->LoadIC_Initialize();
-  RelocInfo::Mode mode = (typeof_state == INSIDE_TYPEOF || var->is_qml_global())
+  RelocInfo::Mode mode = (typeof_state == INSIDE_TYPEOF)
       ? RelocInfo::CODE_TARGET
       : RelocInfo::CODE_TARGET_CONTEXT;
   __ call(ic, mode);
@@ -1224,7 +1224,7 @@ void FullCodeGenerator::EmitVariableLoad(VariableProxy* proxy) {
       __ mov(eax, var->is_qml_global()?QmlGlobalObjectOperand():GlobalObjectOperand());
       __ mov(ecx, var->name());
       Handle<Code> ic = isolate()->builtins()->LoadIC_Initialize();
-      __ call(ic, var->is_qml_global()?RelocInfo::CODE_TARGET:RelocInfo::CODE_TARGET_CONTEXT);
+      __ call(ic, RelocInfo::CODE_TARGET_CONTEXT);
       context()->Plug(eax);
       break;
     }
@@ -2166,7 +2166,7 @@ void FullCodeGenerator::VisitCall(Call* expr) {
   } else if (proxy != NULL && proxy->var()->IsUnallocated()) {
     // Push global object as receiver for the call IC.
     __ push(proxy->var()->is_qml_global()?QmlGlobalObjectOperand():GlobalObjectOperand());
-    EmitCallWithIC(expr, proxy->name(), proxy->var()->is_qml_global()?RelocInfo::CODE_TARGET:RelocInfo::CODE_TARGET_CONTEXT);
+    EmitCallWithIC(expr, proxy->name(), RelocInfo::CODE_TARGET_CONTEXT);
 
   } else if (proxy != NULL && proxy->var()->IsLookupSlot()) {
     // Call to a lookup slot (dynamically introduced variable).

src/3rdparty/v8/src/ia32/lithium-codegen-ia32.cc
@@ -3132,7 +3132,7 @@ void LCodeGen::DoCallGlobal(LCallGlobal* instr) {
   ASSERT(ToRegister(instr->result()).is(eax));
 
   int arity = instr->arity();
-  RelocInfo::Mode mode = instr->qml_global()?RelocInfo::CODE_TARGET:RelocInfo::CODE_TARGET_CONTEXT;
+  RelocInfo::Mode mode = RelocInfo::CODE_TARGET_CONTEXT;
   Handle<Code> ic =
       isolate()->stub_cache()->ComputeCallInitialize(arity, mode);
   __ mov(ecx, instr->name());

src/3rdparty/v8/src/ic.h
@@ -107,10 +107,16 @@ class IC {
   // object that contains this IC site.
   RelocInfo::Mode ComputeMode();
 
+  bool IsQmlGlobal(Handle<Object> receiver) {
+    JSObject* qml_global = isolate_->context()->qml_global();
+    return !qml_global->IsUndefined() && qml_global == *receiver;
+  }
+
   // Returns if this IC is for contextual (no explicit receiver)
   // access to properties.
   bool IsContextual(Handle<Object> receiver) {
-    if (receiver->IsGlobalObject()) {
+    if (receiver->IsGlobalObject() ||
+        IsQmlGlobal(receiver)) {
       return SlowIsContextual();
     } else {
       ASSERT(!SlowIsContextual());

commit 70adcad251ae129c15e5146c54bb3e0c11ee728f

Fix assert in stub-cache.cc when loading qml-mode functions
Commit c511fa8a6a631e45ee4075453bcb2eeb7f01ba63 introduced a
regression that could cause an assert in V8 to be triggered:

Fatal error in ../3rdparty/v8/src/stub-cache.cc, line 1171
CHECK(!CallIC::Contextual::decode( Code::ExtractExtraICStateFromFlags(flags))) failed

The assert would be triggered when 1) the "QML global object"
(implicit receiver in qml-mode evaluation) defined more than 12
properties, and 2) a function property of the QML global object was
looked up at least twice from JavaScript. In particular, the V8 IC
would assert that there should be an explicit receiver when
advancing the function lookup from pre-monomorphic state to
monomorphic state.

(The magic number 12 (number of properties needed to trigger the bug)
is the kMaxFastProperties constant. This is the threshold where V8
switches to using a dictionary for the object storage, instead of
using "fast properties" (array).)

The IC correctly determines that the property holder object (the QML
global object) is not the JS Global Object, and hence compiles it as
a "normal" call, in which it asserts that "there must be an explicit
receiver here". But in QML mode, the receiver can be implicit when
the holder is the QML global object.

Propagate this information to the stub cache to avoid the assert
triggering. The generated JIT code still works as expected when the
receiver is implicit (i.e., the receiver is always loaded).
None of the other property/function-load IC handlers have a similar
assert.

It might be worth investigating whether "QML global object" property
loading should have dedicated cache stubs; e.g. a
ComputeCallQmlGlobal() akin to ComputeCallGlobal(). This could
potentially simplify/speed up the generated code, since we can take
advantage of the read-only nature of the QML global object type right
off the bat. But that's a bigger change that would require all the
JIT compilers and the GC to be adapted.

src/3rdparty/v8/src/ic.cc
@@ -622,7 +622,7 @@ Handle<Code> CallICBase::ComputeMonomorphicStub(LookupResult* lookup,
         // applicable.
         if (!holder.is_identical_to(receiver)) return Handle<Code>::null();
         return isolate()->stub_cache()->ComputeCallNormal(
-            argc, kind_, extra_state);
+            argc, kind_, extra_state, IsQmlGlobal(holder));
       }
       break;
     }

src/3rdparty/v8/src/stub-cache.cc
@@ -757,7 +757,8 @@ Handle<Code> StubCache::ComputeCallPreMonomorphic(
 
 Handle<Code> StubCache::ComputeCallNormal(int argc,
                                           Code::Kind kind,
-                                          Code::ExtraICState extra_state) {
+                                          Code::ExtraICState extra_state,
+                                          bool has_qml_global_receiver) {
   Code::Flags flags =
       Code::ComputeFlags(kind, MONOMORPHIC, extra_state, NORMAL, argc);
   Handle<NumberDictionary> cache = isolate_->factory()->non_monomorphic_cache();
@@ -765,7 +766,7 @@ Handle<Code> StubCache::ComputeCallNormal(int argc,
   if (entry != -1) return Handle<Code>(Code::cast(cache->ValueAt(entry)));
 
   StubCompiler compiler(isolate_);
-  Handle<Code> code = compiler.CompileCallNormal(flags);
+  Handle<Code> code = compiler.CompileCallNormal(flags, has_qml_global_receiver);
   FillCache(isolate_, code);
   return code;
 }
@@ -1162,13 +1163,15 @@ Handle<Code> StubCompiler::CompileCallPreMonomorphic(Code::Flags flags) {
 }
 
 
-Handle<Code> StubCompiler::CompileCallNormal(Code::Flags flags) {
+Handle<Code> StubCompiler::CompileCallNormal(Code::Flags flags, bool has_qml_global_receiver) {
   int argc = Code::ExtractArgumentsCountFromFlags(flags);
   Code::Kind kind = Code::ExtractKindFromFlags(flags);
   if (kind == Code::CALL_IC) {
-    // Call normal is always with a explict receiver.
+    // Call normal is always with a explict receiver,
+    // or with an implicit qml global receiver.
     ASSERT(!CallIC::Contextual::decode(
-        Code::ExtractExtraICStateFromFlags(flags)));
+        Code::ExtractExtraICStateFromFlags(flags)) ||
+        has_qml_global_receiver);
     CallIC::GenerateNormal(masm(), argc);
   } else {
     KeyedCallIC::GenerateNormal(masm(), argc);

src/3rdparty/v8/src/stub-cache.h
@@ -218,7 +218,8 @@ class StubCache {
 
   Handle<Code> ComputeCallNormal(int argc,
                                  Code::Kind kind,
-                                 Code::ExtraICState state);
+                                 Code::ExtraICState state,
+                                 bool has_qml_global_receiver);
 
   Handle<Code> ComputeCallArguments(int argc, Code::Kind kind);
 
@@ -383,7 +384,7 @@ class StubCompiler BASE_EMBEDDED {
   // is extracted from the code flags.
   Handle<Code> CompileCallInitialize(Code::Flags flags);
   Handle<Code> CompileCallPreMonomorphic(Code::Flags flags);
-  Handle<Code> CompileCallNormal(Code::Flags flags);
+  Handle<Code> CompileCallNormal(Code::Flags flags, bool has_qml_global_receiver);
   Handle<Code> CompileCallMegamorphic(Code::Flags flags);
   Handle<Code> CompileCallArguments(Code::Flags flags);
   Handle<Code> CompileCallMiss(Code::Flags flags);

tests/auto/v8/tst_v8.cpp
@@ -62,6 +62,7 @@ private slots:
     void getcallingqmlglobal();
     void typeOf();
     void referenceerror();
+    void qtbug_24871();
 };
 
 void tst_v8::eval()
@@ -102,6 +103,11 @@ void tst_v8::typeOf()
 void tst_v8::referenceerror()
 {
     QVERIFY(v8test_referenceerror());
+}
+
+void tst_v8::qtbug_24871()
+{
+    QVERIFY(v8test_qtbug_24871());
 }
 
 int main(int argc, char *argv[])

tests/auto/v8/v8test.cpp
@@ -493,5 +493,41 @@ cleanup:
     ENDTEST();
 }
 
+bool v8test_qtbug_24871()
+{
+    BEGINTEST();
+
+    HandleScope handle_scope;
+    Persistent<Context> context = Context::New();
+    Context::Scope context_scope(context);
+
+    Local<Object> qmlglobal = Object::New();
+
+    Local<Script> script = Script::Compile(String::New(
+                                               // Create a bunch of properties to exceed kMaxFastProperties
+                                               "var a1, a2, a3, a4, a5, a6, a7, a8;\n"
+                                               "var b1, b2, b3, b4, b5, b6, b7, b8;\n"
+                                               "var c1, c2, c3, c4, c5, c6, c7, c8;\n"
+                                               "var d1, d2, d3, d4, d5, d6, d7, d8;\n"
+                                               "function index(a) { return a + 1; }\n"
+                                               "function init() {\n"
+                                               "  for (var i = 0; i < 300; ++i)\n"
+                                               "    index(i);\n"
+                                               "}\n"
+                                               "init();"), NULL, NULL,
+                                           Handle<String>(), Script::QmlMode);
+
+    TryCatch tc;
+    Local<Value> result = script->Run(qmlglobal);
+
+    VERIFY(!tc.HasCaught());
+    VERIFY(result->IsUndefined());
+
+cleanup:
+    context.Dispose();
+
+    ENDTEST();
+}
+
 #undef VARNAME
 #undef VARVALUE

tests/auto/v8/v8test.h
@@ -56,6 +56,7 @@ bool v8test_globalcall();
 bool v8test_getcallingqmlglobal();
 bool v8test_typeof();
 bool v8test_referenceerror();
+bool v8test_qtbug_24871();
 
 #endif // V8TEST_H
 