989f31e648ef62c8f24015ec0e614dc641b44cbc
Sergio Martins <sergio.martins@kdab.com>
2013-04-15 15:40:16 +0100

    [V8] Fix QNX build due to new CPU profiling methods
    Upstream commit 99a5e7642cc46d72a61693d2d6ffae2108642789 added
    new methods.
    
    This commit adds them to platform-qnx.
    
    Most platforms don't implement them yet. Neither does QNX.
    
    Change-Id: Ifc9f9f950c9c4b068241eeeeea6b25ce715ed328
    Reviewed-by: Nicolas Arnaud-Cormos <nicolas@kdab.com>
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- src/3rdparty/v8/src/platform-qnx.cc
+++ src/3rdparty/v8/src/platform-qnx.cc
@@ -1104,4 +1104,21 @@ void Sampler::Stop() {
 }
 
 
+bool Sampler::CanSampleOnProfilerEventsProcessorThread() {
+  return false;
+}
+
+
+void Sampler::DoSample() {
+}
+
+
+void Sampler::StartProfiling() {
+}
+
+
+void Sampler::StopProfiling() {
+}
+
+
 } }  // namespace v8::internal
